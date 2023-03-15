0d83835d14c118b5e356dbf0d9c8c652c37f7fd7
Teykinay <teykinaycerkezoglu@gmail.com>
2023-03-15 19:36:11 +0300

    login sayfası eklendi ve driver dosyalarında düzeltme yapıldı

--- src/test/java/stepdefinitions/FirstStepDefinition.java
+++ src/test/java/stepdefinitions/FirstStepDefinition.java
@@ -10,14 +10,16 @@ public class FirstStepDefinition {
         System.out.println("Given step i calisti");
         Driver.getDriver().get("https://google.com");
     }
+
     @When("Senaryomu calistirdigimda")
     public void senaryomu_calistirdigimda() {
         System.out.println("When step i calisti");
     }
+
     @Then("Senaryomun calistigini konsolda gormeliyim")
     public void senaryomun_calistigini_konsolda_gormeliyim() {
         System.out.println("Then step i calisti");
     }
 
-        
+
 }
---
+++ src/test/java/stepdefinitions/LoginStepDefinitions.java
@@ -0,0 +1,38 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.Wait;
+import pages.LoginStep;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.time.Duration;
+import java.util.Properties;
+
+public class LoginStepDefinitions {
+    LoginStep ls = new LoginStep(Driver.getDriver());
+    @Given(": Kullanici login butanuna basar")
+    public void kullaniciLoginButanunaBasar() {
+        ls.driverGet("https://qa-gm3.quaspareparts.com/");
+        ls.clickLogin();
+    }
+    @When(": username ve password girişini gerçekleştirir")
+    public void usernameVePasswordGirişiniGerçekleştirir() {
+        String email = ConfigReader.getProperty("email").replaceAll(" ","");
+        String password = ConfigReader.getProperty("password").replaceAll(" ","");
+        System.out.println(email);
+        ls.typeUsername(email);
+        ls.typePassword(password);
+        ls.clickSubBtn();
+    }
+
+    @Then(":Profil simgesine gider ve Account Manegement sekmesini açar")
+    public void profilSimgesineGiderVeAccountManegementSekmesiniAçar() {
+        Duration.ofSeconds(10);
+        ls.clickprofile();
+        ls.clickAccount_mng();
+    }
+}
