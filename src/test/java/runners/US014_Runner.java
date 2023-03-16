0ca512997000381def220a5db2aaaf60a21886cf
mehmet20251990 <mehmet20251990@gmail.com>
2023-03-16 22:57:29 +0300

    update teams module

--- src/test/java/runners/US014_Runner.java
+++ src/test/java/runners/US014_Runner.java
@@ -1,4 +1,16 @@
 package runners;
 
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+@RunWith(Cucumber.class)
+// raporlari olustururken format:path seklinde raporun nerede ve hangi formatta olusturacagimizi belirtiriz
+@CucumberOptions(
+        features = {"src/test/resources/features/US014.feature"},
+        glue = {"US014_Stepdefs", "hooks"},
+        plugin = {"pretty"},
+        monochrome = true
+        )
 public class US014_Runner {
+
 }
