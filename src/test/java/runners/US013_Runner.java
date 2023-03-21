a956d1837a4261f7ad47259ba2aa438284cf8b39
mehmet20251990 <mehmet20251990@gmail.com>
2023-03-21 20:48:35 +0300

    teams update

--- src/test/java/runners/US013_Runner.java
+++ src/test/java/runners/US013_Runner.java
@@ -4,10 +4,9 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-// raporlari olustururken format:path seklinde raporun nerede ve hangi formatta olusturacagimizi belirtiriz
 @CucumberOptions(
         features = {"src/test/resources/features/US013.feature"},
-        glue = {"US013_Stepdefs", "hooks"},
+        glue = {"stepdefinitions", "hooks"},
         plugin = {"pretty"},
         tags = "@FirstHooks"
         )
